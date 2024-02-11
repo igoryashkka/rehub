0e9d4a7354482e28eef43c0f2446009be5120eb7
igoryashkka <igor404ik@gmail.com>
2024-02-11 03:10:15 +0200

    add html , edit urls, inital views.

--- forum/urls.py
+++ forum/urls.py
@@ -5,5 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('',Home.as_view(), name = 'home'),
+    path('projects/',Projects.as_view(),name = 'about'),
     path('about/',about,name = 'about'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -5,6 +5,7 @@ from django.views.generic.list import ListView
 from forum.models import Post
 # Create your views here.
 
+projects = list(range(1,11))
 
 class Home(ListView):
     model = Post
@@ -16,6 +17,12 @@ class Home(ListView):
         context =  super().get_context_data(**kwargs)
         return context
 
+
+class Projects(ListView):
+    model = Post 
+    template_name = 'forum/projects.html'
+    context_object_name = 'records'
+    extra_context = {'projects':projects}# extra_context - only for static data
 
 def about(request):
     return render(request,'forum/about.html', {'title':'Text Title About','list_data':'nonen'})
